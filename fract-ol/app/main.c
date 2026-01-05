8de1be4acbbe7c4347a11b8dad2fd2b74cba2786
msisdev <msisdev@gmail.com>
2026-01-05 14:29:30 +0900

    rename t_point > t_point2d

--- fract-ol/app/main.c
+++ fract-ol/app/main.c
@@ -6,13 +6,14 @@
 /*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 19:05:19 by minseobk          #+#    #+#             */
-/*   Updated: 2026/01/04 19:25:45 by minseobk         ###   ########.fr       */
+/*   Updated: 2026/01/05 14:23:51 by minseobk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+#include <stdio.h>
 
-int	handle_close(int keycode, t_context *c)
+static int	handle_key_press(int keycode, t_context *c)
 {
 	(void)keycode;
 	mlx_destroy_window(c->mlx, c->win);
@@ -20,15 +21,29 @@ int	handle_close(int keycode, t_context *c)
 	return (0);
 }
 
+static int	handle_loop(t_context *c)
+{
+	int	t;
+
+	t = c->s.frame + 1;
+	if (t == 60)
+		t = 0;
+	if (t % 10 == 0)
+		printf("frame: %2d\n", t);
+	c->s.frame = t;
+	return (0);
+}
+
 int	main(void)
 {
-	const t_point	center = {.x = WINDOW_W / 2, .y = WINDOW_H / 2};
+	const t_point2d	center = {.x = WINDOW_W / 2, .y = WINDOW_H / 2};
 	t_context		c;
 
 	ctx_init(&c);
 	draw_circle(&c.d, center, 200, PALETTE_MAGENTA);
 	ctx_display(&c);
-	ctx_hook(&c, EVENT_KEY_PRESS, MASK_KEY_PRESS, handle_close);
+	ctx_hook_event(&c, EVENT_KEY_PRESS, MASK_KEY_PRESS, handle_key_press);
+	ctx_hook_loop(&c, handle_loop);
 	ctx_loop(&c);
 	return (0);
 }
